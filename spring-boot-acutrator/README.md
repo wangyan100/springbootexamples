d3eff499b3474b6640fbd9115927e25456dc456a
wangyan100 <wyanbox@gmail.com>
2019-06-01 20:54:36 +0200

    update

---
+++ spring-boot-acutrator/README.md
@@ -0,0 +1,46 @@
+
+
+# Spring Boot Actuator 
+
+This demo shows you how to use Spring Boot Actuator to monitor your Application
+
+We will use SpringBoot+ElastichSearch as example , which you could find source code at
+
+https://github.com/wangyan100/springbootexamples/tree/master/spring-boot-elasticsearch 
+
+
+
+- Using Acutator 
+  
+  - import dependency  
+  
+    ![1](/Users/yanwang/project/springbootexamples/spring-boot-acutrator/screenshot/1.png)
+  
+    
+  
+  - Config Endpoint
+  
+    ![2](/Users/yanwang/project/springbootexamples/spring-boot-acutrator/screenshot/2.png)
+  
+    
+  
+  - start elastichsearch and SpringBootApplication, you could use Actuator as below to monitor Application running status as below shown![3](/Users/yanwang/project/springbootexamples/spring-boot-acutrator/screenshot/3.png)
+  
+  - it shows application info![4](/Users/yanwang/project/springbootexamples/spring-boot-acutrator/screenshot/4.png)
+  
+    
+  
+  - it shows elasticsearch is up and running , diskspace and so on. 
+  
+    ![5](/Users/yanwang/project/springbootexamples/spring-boot-acutrator/screenshot/5.png)
+
+
+
+
+
+### To implement your own micrometer 
+
+- 
+
+
+
---
+++ spring-boot-elasticsearch/src/main/java/org/yw/springbootelasticsearch/indicator/HandyIndicator.java
@@ -0,0 +1,45 @@
+package org.yw.springbootelasticsearch.indicator;
+
+
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchType;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class HandyIndicator implements HealthIndicator {
+
+    @Autowired
+    Client client;
+    @Override
+    public Health health() {
+        SearchResponse response = client.prepareSearch("handys")
+                .setTypes("handy")
+                .setSearchType(SearchType.QUERY_THEN_FETCH)
+                .setQuery(QueryBuilders.matchAllQuery())
+                .get();
+        List<SearchHit> searchHits = Arrays.asList(response.getHits().getHits());
+        int handyCount=searchHits.size();
+        Health health;
+        if (handyCount > 0) {
+            health = Health.up()
+                    .withDetail("count", handyCount)
+                    .withDetail("message", "We have enough Handys.")
+                    .build();
+        } else {
+            health = Health.down()
+                    .withDetail("count", 0)
+                    .withDetail("message", "We have no enough Handys.")
+                    .build();
+        }
+        return health;
+    }
+}
